f5fab39c946119c8819f5d6d9330facd31850827
SammyBolger <123004016+SammyBolger@users.noreply.github.com>
2024-02-22 13:07:03 -0600

    Add files via upload

---
+++ Game/src/Entity/Player.java
@@ -0,0 +1,20 @@
+package Entity;
+
+import Main.GamePanel;
+import Main.KeyHandler;
+
+public class Player extends Entity{
+
+	GamePanel gp;
+	KeyHandler keyH;
+	
+	public Player(GamePanel gp, KeyHandler keyH) {
+		this.gp = gp;
+		this.keyH = keyH;
+		
+	}
+	
+	
+	
+}
+
